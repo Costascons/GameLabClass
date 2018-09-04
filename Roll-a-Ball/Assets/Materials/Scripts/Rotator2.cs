fb7452f38c440c57602cec9250d7cdf25f2e5303
Costascons <consc177@newschool.edu>
2018-09-04 16:43:32 -0400

    adding projects to github

---
+++ Roll-a-Ball/Assets/Materials/Scripts/Rotator2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator2 : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () 
+        {
+            transform.Rotate(new Vector3(45, 55, 70) * Time.deltaTime);
+        }
+    }
+
+
