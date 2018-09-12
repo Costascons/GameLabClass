a99abdb520f9bfbcf311233cb37187b023d86eda
Costascons <consc177@newschool.edu>
2018-09-12 00:06:04 -0400

    Repository added
    Repository added

---
+++ Physics/Assets/Physics.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics : MonoBehaviour {
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+    void FixedUpdate()
+    {
+    }
+        void OnCollisionEnter(Collision other)
+        {
+            //Debug.Log("i touched something!!");
+            Debug.Log(other.gameObject.tag);
+            if (other.gameObject.tag == "Sphere")
+            {
+
+                gameObject.GetComponent<Renderer>().material.color = Color.green;
+
+
+            }
+        }
+        void OnCollisionStay(Collision other)
+        {
+            //Debug.Log("i'm still touching!!");
+            if (other.gameObject.tag == "Sphere") {
+
+                gameObject.GetComponent<Renderer>().material.color = Color.green;
+                GetComponent<Rigidbody>().AddForce(Vector3.up * upforce);
+            }
+        }
+        void OnCollisionExit(Collision other)
+        {
+            //Debug.Log("no longer touching");
+            if (other.gameObject.tag == "Sphere")
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+
+    
+  
+    public float upforce;
+
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * upforce);
+        GetComponent<Rigidbody>().AddTorque(Vector3.up * upforce);
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+
+    }
+    void OnTriggerStay(Collider other)
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * upforce);
+        GetComponent<Rigidbody>().AddTorque(Vector3.up * upforce);
+
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+
+
+
+}
