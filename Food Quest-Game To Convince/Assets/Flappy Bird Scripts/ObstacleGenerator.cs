406e7fec7bf409b11010420bd64c993ca994e78f
Costascons <consc177@newschool.edu>
2018-10-18 18:13:07 -0400

    commiting

---
+++ Food Quest-Game To Convince/Assets/Flappy Bird Scripts/ObstacleGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public GameObject platform;
+    float timer = 0;
+
+    GameObject[] platforms;
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+        if (timer >= 0.05)
+        {
+            
+            GameObject clone = Instantiate(platform, (new Vector2(Random.Range(3f, 300f), 10f)), Quaternion.identity);
+            clone.transform.localScale = new Vector2(Random.Range(0.5f, 1f), 0.5f);
+            timer = 0;
+        }
+        platforms = GameObject.FindGameObjectsWithTag("platform");
+
+        foreach (GameObject platform in platforms)
+        {
+            platform.transform.localPosition = new Vector2(platform.transform.localPosition.y, platform.transform.localPosition.x - 0.1f);
+            {
+                if (platform.transform.localPosition.x < -6)
+                {
+                    Destroy(platform);
+                }
+
+            }
+
+
+        }
+        
+    }
+}
---
+++ Food Quest-Game To Convince/Assets/Flappy Bird Scripts/characMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characMovement : MonoBehaviour
+{
+    public float speed = 3f;
+    public float jumpSpeed = 10f;
+    private float movement = 0f;
+    private Rigidbody2D rigidBody;
+  
+    
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        movement = Input.GetAxis("Horizontal");
+        
+        if (movement > 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+            transform.localScale = new Vector2(1f, 1f);
+        }
+        else if (movement < 0f)
+        {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+            transform.localScale = new Vector2(-1f, 1f);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+        }
+    }
+}
