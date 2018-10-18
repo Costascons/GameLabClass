1fb94dd43d81a02b5f6736963ac7d3ebdde61ef0
Costascons <consc177@newschool.edu>
2018-10-18 18:20:26 -0400

    commiting

---
+++ Flappy Bird Adaptation/Assets/Flappy Bird Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+    private bool finished = false;
+    
+    void Start()
+    {
+        
+        startTime = Time.time;
+
+    }
+
+    
+    void Update()
+    {
+        if (finished) return;
+        float t = Time.time - startTime;
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+        
+    }
+    public void Finished()
+    {
+        finished = true;
+        timerText.color = Color.red;
+
+    }
+}
